91131f2fd44c559c3029aa5de3d4d262fbf23989
Joe Stanton <joe.stanton@fruition.net>
2023-02-20 21:08:03 -0500

    more updates

--- src/components/Blocks/Hero/Hero.js
+++ src/components/Blocks/Hero/Hero.js
@@ -3,16 +3,19 @@ import Link from 'next/link';
 
 const Hero = ({ props }) => {
 
-	const { title, copy, backgroundImage, cta  } = props;
+	const { title, copy, cta  } = props;
 
 	return (
-		<div className={styles.heroFluid} style={{backgroundImage: "url(" + backgroundImage.sourceUrl + ")"}}>
+		<div className={styles.heroFluid}>
 			<div className="container">
 				<div className="grid">
 					<div className="col-sm-6">
 						<h1>{title}</h1>
 						<p>{copy}</p>
 						<Link href={cta.url}>{cta.title}</Link>
+					</div>
+					<div className="col-sm-6">
+
 					</div>
 				</div>
 			</div>
